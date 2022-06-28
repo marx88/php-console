3afef0bdcfd2080d7ebbbedc7eb0db9a22e1f819
Marx <marx_88@126.com>
2022-06-28 17:00:37 +0800

    重设颜色默认值

--- src/Color.php
+++ src/Color.php
@@ -10,80 +10,83 @@ namespace Marx\Console;
 class Color
 {
     /**
-     * 灰色.
+     * 灰色 0=不高亮 1=高亮.
      */
-    public static function grey(string $text): string
+    public static function grey(string $text, int $light = 1): string
     {
-        return static::fmt(__FUNCTION__, $text);
+        return static::fmt(__FUNCTION__, $text, $light);
     }
 
     /**
-     * 红色.
+     * 红色 0=不高亮 1=高亮.
      */
-    public static function red(string $text): string
+    public static function red(string $text, int $light = 1): string
     {
-        return static::fmt(__FUNCTION__, $text);
+        return static::fmt(__FUNCTION__, $text, $light);
     }
 
     /**
-     * 绿色.
+     * 绿色 0=不高亮 1=高亮.
      */
-    public static function green(string $text): string
+    public static function green(string $text, int $light = 1): string
     {
-        return static::fmt(__FUNCTION__, $text);
+        return static::fmt(__FUNCTION__, $text, $light);
     }
 
     /**
-     * 黄色.
+     * 黄色 0=不高亮 1=高亮.
      */
-    public static function yellow(string $text): string
+    public static function yellow(string $text, int $light = 1): string
     {
-        return static::fmt(__FUNCTION__, $text);
+        return static::fmt(__FUNCTION__, $text, $light);
     }
 
     /**
-     * 蓝色.
+     * 蓝色 0=不高亮 1=高亮.
      */
-    public static function blue(string $text): string
+    public static function blue(string $text, int $light = 1): string
     {
-        return static::fmt(__FUNCTION__, $text);
+        return static::fmt(__FUNCTION__, $text, $light);
     }
 
     /**
-     * 紫色.
+     * 紫色 0=不高亮 1=高亮.
      */
-    public static function purple(string $text): string
+    public static function purple(string $text, int $light = 1): string
     {
-        return static::fmt(__FUNCTION__, $text);
+        return static::fmt(__FUNCTION__, $text, $light);
     }
 
     /**
-     * 青色.
+     * 青色 0=不高亮 1=高亮.
      */
-    public static function cyan(string $text): string
+    public static function cyan(string $text, int $light = 1): string
     {
-        return static::fmt(__FUNCTION__, $text);
+        return static::fmt(__FUNCTION__, $text, $light);
     }
 
     /**
-     * 白色.
+     * 白色 0=不高亮 1=高亮.
      */
-    public static function white(string $text): string
+    public static function white(string $text, int $light = 1): string
     {
-        return static::fmt(__FUNCTION__, $text);
+        return static::fmt(__FUNCTION__, $text, $light);
     }
 
-    private static function fmt(string $color, string $text): string
+    private static function fmt(string $color, string $text, int $light): string
     {
+        if (0 !== $light) {
+            $light = 1;
+        }
         $map = [
-            'grey' => '0;30m',
-            'red' => '0;31m',
-            'green' => '0;32m',
-            'yellow' => '0;33m',
-            'blue' => '0;34m',
-            'purple' => '0;35m',
-            'cyan' => '0;36m',
-            'white' => '0;37m',
+            'grey' => "{$light};30m",
+            'red' => "{$light};31m",
+            'green' => "{$light};32m",
+            'yellow' => "{$light};33m",
+            'blue' => "{$light};34m",
+            'purple' => "{$light};35m",
+            'cyan' => "{$light};36m",
+            'white' => "{$light};37m",
         ];
 
         return "\033[".$map[$color].$text."\033[0m";
